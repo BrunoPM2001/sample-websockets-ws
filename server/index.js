5094803fc61d8c05d0b2a9380a5a35580b6e6a36
BrunoPM2001 <brunoporras2001@gmail.com>
2023-02-19 15:30:39 -0500

    Cerrar conexión y cuenta de clientes añadido

--- server/index.js
+++ server/index.js
@@ -4,14 +4,25 @@ const server = new WebSocketServer({
   port: "8080",
 });
 
-server.on("connection", (socket) => {
+server.on("connection", (socket, req) => {
+  console.log(`New client is connected, ip: ${req.socket.remoteAddress}`);
+
+  //  Message from client
   socket.on("message", (message) => {
     server.broadcast(message);
   });
+
+  //  Close connections
+  socket.on("close", () => {
+    console.log("A client got disconnected :(");
+  });
+
+  //  Errors
+  socket.on("error", console.error);
 });
 
 server.broadcast = (msg) => {
-  console.log("Message: ", msg.toString());
+  console.log(`Sending ${server.clients.size} messages`);
   server.clients.forEach((client) => {
     client.send(`${msg}`);
   });
